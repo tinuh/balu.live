5503515c314240af0f0f18e9a0887651933d0dd8
tinu4 <tinuvanapamula@gmail.com>
2020-09-15 13:16:05 -0400

    Updates

---
+++ precache-manifest.38f59f3a8c85aed9d335b73e74eee1b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee1d2c1c4c736a14e97960bda926a1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3a77eb1af90f56e0a30",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "498cce04ec41f736bee6",
+    "url": "/static/js/2.39c6ceba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.39c6ceba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3a77eb1af90f56e0a30",
+    "url": "/static/js/main.77d8eefd.chunk.js"
+  },
+  {
+    "revision": "b1202c90baa2feb5895e",
+    "url": "/static/js/runtime-main.4fafa45f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
