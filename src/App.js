bcac967a1c35371668eb01d449eef7b9fbe03cc9
tinu4 <tinuvanapamula@gmail.com>
2020-09-15 14:06:53 -0400

    transition to react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Fragment} from "react";
+import {Router, Switch} from "react-router-dom";
+
+function App() {
+  return (
+    <Fragment className="App">
+      <Router>
+
+      </Router>
+    </Fragment>
+  );
+}
+
+export default App;
